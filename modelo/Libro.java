9cf50b2d75557d4ee746c2328355476a1a39dbe5
Gustavo Rojas Peña <2223010469@untels.edu.pe>
2025-04-08 15:37:37 -0500

    clase modelo listo

---
+++ modelo/Libro.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Libro{
+	private String titulo;
+	public void setNombre(String nombre){
+	
+}
+	public String getNombre(){
+	
+}
+}
--- modelo/Persona.java
+++ modelo/Persona.java
@@ -3,9 +3,9 @@ package modelo;
 public class Persona{
 	private String nombre;
 	public void setNombre(String nombre){
-	xxxxx
+	
 }
 	public String getNombre(){
-	yyyyyy 
+	
 }
 }
